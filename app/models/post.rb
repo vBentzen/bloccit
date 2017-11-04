da478c673dba66adb490a14fec547ba4d5102751
Aske Bentzen <askebentzen@gmail.com>
2017-11-04 09:47:32 +0100

    added authentication for signing up users

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 
   validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
+  validates :body,  length: { minimum: 20 }, presence: true
   validates :topic, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029111119) do
+ActiveRecord::Schema.define(version: 20171104082406) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20171029111119) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
