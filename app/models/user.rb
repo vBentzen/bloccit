8cfcdfdd70d78cc6efd1505f0fa955e5d8763484
Aske Bentzen <askebentzen@gmail.com>
2017-11-04 10:01:33 +0100

    finished assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
-
+  before_save :format_name
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  def format_name
+    name_array = []
+    name.split.each do |name_part|
+      name_array << name_part.capitalize
+    end
+    self.name = name_array.join(" ")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should format the user's name" do
+      user.name = "bloccit user"
+      user.save
+      expect(user.name).to eq ("Bloccit User")
+    end
   end
 
   describe "invalid user" do
