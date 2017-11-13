92782b39f03aa66008212768921ba923cc6c71eb
Aske Bentzen <askebentzen@gmail.com>
2017-11-13 18:33:20 +0100

    added function to vote on posts

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Vote, type: :model do
+  let(:topic) { Topic.create!(name: Faker::StarWars.character, description: Faker::Lorem.paragraph) }
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:post) { topic.posts.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, user: user) }
+  let(:vote) { Vote.create!(value: 1, post: post, user: user) }
+
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to belong_to(:user) }
+
+  it { is_expected.to validate_presence_of(:value) }
+  it { is_expected.to validate_inclusion_of(:value).in_array([-1, 1]) }
+
+  describe "update_post callback" do
+    it "triggers update_post on save" do
+      expect(vote).to receive(:update_post).at_least(:once)
+      vote.save!
+    end
+
+    it "#update_post should call update_rank on post " do
+      expect(post).to receive(:update_rank).at_least(:once)
+      vote.save!
+    end
+  end
+end
